8b24921a3bb54312452d348b83737f2f8edaa3bc
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-15 17:02:50 +0000

    checking it works

--- starter-code copy/src/app.js
+++ starter-code copy/src/app.js
@@ -10,9 +10,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-
+        <h1>still working</h1>
         <div className="container">
-    
+          <h1>working</h1>
           <h1>Siobhan Potter1</h1>
           <h2>Web Developer</h2>
           <p>Hi! I'm Siobhan, I'm a web developer based in London. Check out my work, find out more about me or get in touch. I'd love to hear from you.</p>
