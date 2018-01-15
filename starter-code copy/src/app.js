879b1a34cb8b98cd767778b16b0cc683b6e457cd
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-15 19:01:50 +0000

    added some sections and styling

--- starter-code copy/src/app.js
+++ starter-code copy/src/app.js
@@ -10,44 +10,52 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
+        <section className="header">
+          <div className="container title">
+            <h4>SIOBHAN POTTER</h4>
+            <h6>Web Developer | Creative | Music Lover</h6>
+          </div>
+          <div className="intro">
+            <p>Hi! I'm Siobhan,<br/> I'm a web developer based in London. Check out my work, find out more about me & get in touch.</p>
+          </div>
+        </section>
 
+        <section className="technologies">
+          <h1>Technologies</h1>
+        </section>
 
-
-        <div className="header">
-          <h1>Siobhan Potter</h1>
-          <h2>Web Developer</h2>
-          <p>Hi! I'm Siobhan, I'm a web developer based in London. Check out my work, find out more about me or get in touch. I'd love to hear from you.</p>
-        </div>
-        <div className="container main">
-          <div>
+        <section className="container main">
+          {/* <div>
             <h1>Projects</h1>
+          </div> */}
+
+          <div className="row">
+
+            <div className="col-lg-6 col-sm-12 project4 project">
+              <img className="project-image" src="../assets/music-box.png"></img>
+            </div>
+            <div className="col-lg-6 col-sm-12 project3 project">
+              <img className="project-image" src="../assets/gigly.png"></img>
+            </div>
           </div>
           <div className="row">
-            <div className="col project1">
-              1 of 2
+            <div className="col-lg-6 col-sm-12 project2 project">
+              <img className="project-image" src="../assets/viewr.png"></img>
             </div>
-            <div className="col project2">
-              2 of 2
-            </div>
-          </div>
-          <div className="row">
-            <div className="col project3">
-              1 of 2
-            </div>
-            <div className="col project4">
-              2 of 2
+            <div className="col-lg-6 col-sm-12 project1 project">
+              <img className="project-image" src="../assets/unjumble.png"></img>
             </div>
           </div>
-        </div>
+        </section>
 
 
-        <div className="about-me">
+        <section className="about-me">
           <h1>About Me</h1>
-        </div>
+        </section>
 
-        <div className="contact">
+        <section className="contact">
           <h1>Contact Me</h1>
-        </div>
+        </section>
       </div>
 
     );
